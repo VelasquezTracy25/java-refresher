f5c1aaadd6d26e4428d57fd887679a5e451f3c76
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-10-15 17:36:22 -0500

    getters and setters refresher with IllegalArg exception

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Employee employee1 = new Employee();
-        employee1.baseSalary = 50_000;
-        employee1.hourlyRate = 20;
+        employee1.setBaseSalary(50000);
+        employee1.setHourlyRate(50);
         int wage = employee1.calculateWage(30);
         System.out.println(wage);
     }
