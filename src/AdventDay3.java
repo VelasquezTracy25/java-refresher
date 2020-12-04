066cc97b150883c642811f3d0d36ba4500cd5822
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-12-03 23:16:37 -0600

    setup

--- src/AdventDay3.java
+++ src/AdventDay3.java
@@ -1,2 +1,10 @@
-public class AdventDay3 {
+//Write a function that splits an array (first arg) into groups the length of size (second arg) and returns them as a two dimensional array.
+//
+ public class AdventDay3 {
+
+    public static void main(String[] args) {
+
+    }
+
+
 }
