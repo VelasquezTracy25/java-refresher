4aa60007e181867c3475a40028212af5040a7a28
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-12-05 17:40:39 -0600

    solution

--- src/AdventDay5.java
+++ src/AdventDay5.java
@@ -2,5 +2,22 @@
 //"hello" returns "olleh"
 //"Howdy" returns "ydwoH"
 
- public class AdventDay5 {
+public class AdventDay5 {
+
+    public static void main(String[] args) {
+        System.out.println(reverseString("Tracy"));
+        System.out.println(reverseString("hello"));
+        System.out.println(reverseString("Howdy"));
+    }
+
+    public static StringBuilder reverseString(String str) {
+
+        StringBuilder newStr = new StringBuilder();
+
+        newStr.append(str);
+
+        StringBuilder newString = newStr.reverse();
+
+        return newString;
+    }
 }
