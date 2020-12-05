3c7fc593b4f2799ad9adc33d5cc081665964612f
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-12-04 22:07:57 -0600

    setup for problem

--- src/AdventDay4.java
+++ src/AdventDay4.java
@@ -1,2 +1,13 @@
+//Given a year, return the century its in.
+
 public class AdventDay4 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int returnCentury(int year){
+
+    return century;
+    }
 }
