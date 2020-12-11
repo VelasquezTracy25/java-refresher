65584f478558f44ddea46295a1cca8eeb5aea0b9
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-12-10 21:27:47 -0600

    advent day 7

--- src/AdventDay6.java
+++ src/AdventDay6.java
@@ -1,2 +1,14 @@
-public class AdventDay6 {
-}
+////Given an array of strings, sort them in order of increasing lengths. If two strings have the same length, their relative order must be the same as the initial array.
+//
+//import java.util.Arrays;
+//
+//public class AdventDay6 {
+//    public static void main(String[] args) {
+//
+//    }
+//
+//    public static String[] returnSorted(String[] W){
+//        Arrays.sort(W, (a, b)->Integer.compare(a.length(), b.length()));
+//    }
+//
+// }
---
+++ src/AdventDay7.java
@@ -0,0 +1,2 @@
+public class AdventDay7 {
+}
