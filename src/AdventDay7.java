e551134bdf025d0d4b962ddb404f740cef340c0c
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-12-10 21:42:11 -0600

    setup

--- src/AdventDay7.java
+++ src/AdventDay7.java
@@ -1,2 +1,18 @@
+//You are given a string s that consists of only lowercase English letters. If vowels aeiou are given a value of one and consonants are given a value of 2, return the sum of all  the letters in the input string.
+
+
 public class AdventDay7 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int countVowelsConsonants(String string) {
+
+    int count = 0;
+
+
+
+return count;
+    }
 }
