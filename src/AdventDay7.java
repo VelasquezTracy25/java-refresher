513328ccdfabcccfe61d8ea181feb9178fb8e40c
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-12-10 22:03:28 -0600

    tested and completed

--- src/AdventDay7.java
+++ src/AdventDay7.java
@@ -1,10 +1,17 @@
 //You are given a string s that consists of only lowercase English letters. If vowels aeiou are given a value of one and consonants are given a value of 2, return the sum of all  the letters in the input string.
 
 
+import java.util.Arrays;
+
 public class AdventDay7 {
 
     public static void main(String[] args) {
         System.out.println(countVowelsConsonants("Tracy"));
+        System.out.println(countVowelsConsonants("mom"));
+        System.out.println(countVowelsConsonants("hello"));
+        System.out.println(countVowelsConsonants("HELLO"));
+
+
 
     }
 
@@ -12,15 +19,18 @@ public class AdventDay7 {
 
         int count = 0;
 
-        String[] stringToArray = string.split("", 100);
+//        String[] stringToArray = string.split("", 100); //is not case sensitive
+
+        String[] stringToArray = string.toLowerCase().split("", 100);
+//        System.out.println(Arrays.toString(stringToArray));
 
         for (String character : stringToArray) {
-            if (character == "a" || (character == "e") || (character == "i") || (character == "o") || (character == "u")) {
+            if (character.equals("a") || character.equals("e") || character.equals("i") || character.equals("o") || character.equals("u")) {
                 count++;
             } else {
                 count += 2;
             }
-
+//            System.out.println("count is " + count);
         }
         return count;
     }
