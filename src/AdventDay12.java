f3983f3e02f36b3b847c18d6d3d906bf21f55203
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-12-12 22:59:22 -0600

    solution

--- src/AdventDay12.java
+++ src/AdventDay12.java
@@ -1,2 +1,20 @@
+// Check if the given string is a correct time representation of the 24 hour clock
+//
+
 public class AdventDay12 {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isValidTime(String string) {
+        String[] splitTime = string.split(":", 2);
+        int hour = Integer.parseInt(splitTime[0]);
+        int minute = Integer.parseInt(splitTime[1]);
+
+        if (hour < 24 && minute < 60) {
+            return true;
+        }
+        return false;
+    }
+
 }
