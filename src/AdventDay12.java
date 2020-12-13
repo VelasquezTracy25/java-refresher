9bdb48cbe79386ebe73c80070d0a69ec99dbdf01
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-12-12 23:03:39 -0600

    testing edge case for 24:00 and 24:01+

--- src/AdventDay12.java
+++ src/AdventDay12.java
@@ -3,9 +3,11 @@
 
 public class AdventDay12 {
     public static void main(String[] args) {
-    System.out.println(isValidTime("13:58"));
-    System.out.println(isValidTime("25:51"));
-    System.out.println(isValidTime("02:76"));
+        System.out.println(isValidTime("13:58"));
+        System.out.println(isValidTime("25:51"));
+        System.out.println(isValidTime("02:76"));
+        System.out.println(isValidTime("24:01"));
+        System.out.println(isValidTime("24:00"));
     }
 
     public static boolean isValidTime(String string) {
@@ -13,7 +15,9 @@ public class AdventDay12 {
         int hour = Integer.parseInt(splitTime[0]);
         int minute = Integer.parseInt(splitTime[1]);
 
-        if (hour <= 24 && minute < 60) {
+        if (hour <= 23 && minute < 60) {
+            return true;
+        } else if (hour == 24 && minute == 00) {
             return true;
         }
         return false;
