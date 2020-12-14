48e728bd1613f44cbce623924c337a23a4a0ba25
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-12-13 21:42:31 -0600

    day 13 setup

---
+++ src/AdventDay13.java
@@ -0,0 +1,12 @@
+//Given array of integers, remove each kth elemnt from it.
+
+ public class AdventDay13 {
+     public static void main(String[] args) {
+
+     }
+
+     public static int[] extractEachKth(int[] nums, int index){
+
+     return newArray;
+     }
+}
