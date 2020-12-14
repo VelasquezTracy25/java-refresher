8e8989a19480deed2d186025b41217e666af3b09
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-12-13 22:00:47 -0600

    solution

--- src/AdventDay13.java
+++ src/AdventDay13.java
@@ -1,12 +1,26 @@
 //Given array of integers, remove each kth elemnt from it.
 
+import java.util.ArrayList;
+import java.util.List;
+
  public class AdventDay13 {
+
+
      public static void main(String[] args) {
+         ArrayList<Integer> nums = new ArrayList<>();
+         nums.add(1);
+         nums.add(2);
+         nums.add(3);
+         nums.add(4);
+         nums.add(5);
+         System.out.println(extractEachKth(nums, 0));
+
 
      }
 
-     public static int[] extractEachKth(int[] nums, int index){
-
-     return newArray;
+     public static List<Integer> extractEachKth(List<Integer> nums, int index){
+         System.out.println(nums);
+         nums.remove(index);
+     return nums;
      }
 }
