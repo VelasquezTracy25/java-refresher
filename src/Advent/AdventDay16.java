c541520655c9aeda09bcdc74512689a2d22f6c1d
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-12-16 21:58:34 -0600

    temporary solution

--- src/Advent/AdventDay16.java
+++ src/Advent/AdventDay16.java
@@ -5,12 +5,18 @@ package Advent;
 
 // "aba caba" returns "a-b-a c-a-b-a"
 
+import java.util.Arrays;
+
 public class AdventDay16 {
     public static void main(String[] args) {
 
+        System.out.println(addDashes("Tracy is cool."));
     }
 
-    public static String addDashes(String origina){
+    public static String addDashes(String original){
+
+    String[] splitString = original.split("");
+        String newString = String.join("-", splitString);
 
     return newString;
 
