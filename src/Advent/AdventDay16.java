dc7ef05889bccbd7ec1783ee9adec48014ab25e2
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-12-16 21:49:56 -0600

    advent day 16

---
+++ src/Advent/AdventDay16.java
@@ -0,0 +1,9 @@
+package Advent;
+
+//insert dashes
+// Transform a given sentence into a new on with dashes between every two consecutive letters
+
+// "aba caba" returns "a-b-a c-a-b-a"
+
+public class AdventDay16 {
+}
