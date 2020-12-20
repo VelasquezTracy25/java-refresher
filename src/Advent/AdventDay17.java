e87d1b8ccb5ecd0ed2aa30488d08e45013c1b707
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-12-19 23:52:44 -0600

    solution

--- src/Advent/AdventDay17.java
+++ src/Advent/AdventDay17.java
@@ -2,13 +2,34 @@ package Advent;
 
 //Given a string, find the number of different characters in it.
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class AdventDay17 {
     public static void main(String[] args) {
+        System.out.println(countChars("Tracy"));
+        System.out.println(countChars("Marry"));
+        System.out.println(countChars("eeeee"));
 
     }
 
-    public static int countChars(){
+    public static int countChars(String input){
 
-    return chars;
+        char inputArray[] = input.toCharArray();
+        Arrays.sort(inputArray);
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char character : inputArray){
+            if(map.containsKey(character)) {
+                int counter = map.get(character);
+                map.put(character, ++counter);
+            } else {
+                map.put(character, 1);
+            }
+        }
+
+    return map.size();
     }
 }
