02964f6b020627db176be647130c9072caba6eb5
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-12-19 23:39:01 -0600

    setup for porblem

--- src/Advent/AdventDay17.java
+++ src/Advent/AdventDay17.java
@@ -1,4 +1,14 @@
 package Advent;
 
+//Given a string, find the number of different characters in it.
+
 public class AdventDay17 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int countChars(){
+
+    return chars;
+    }
 }
