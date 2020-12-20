3b741b6fae1e857a1fbecf45daed99e879a86103
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-12-19 23:37:39 -0600

    advent day 17

--- src/Advent/AdventDay16.java
+++ src/Advent/AdventDay16.java
@@ -1,24 +1,32 @@
-package Advent;
-
-//insert dashes
-// Transform a given sentence into a new on with dashes between every two consecutive letters
-
-// "aba caba" returns "a-b-a c-a-b-a"
-
-import java.util.Arrays;
-
-public class AdventDay16 {
-    public static void main(String[] args) {
-
-        System.out.println(addDashes("Tracy is cool."));
-    }
-
-    public static String addDashes(String original){
-
-    String[] splitString = original.split("");
-        String newString = String.join("-", splitString);
-
-    return newString;
-
-    }
-}
+//package Advent;
+//
+////insert dashes
+//// Transform a given sentence into a new on with dashes between every two consecutive letters
+//
+//// "aba caba" returns "a-b-a c-a-b-a"
+//
+//import java.util.Arrays;
+//
+//public class AdventDay16 {
+//    public static void main(String[] args) {
+//
+//        addDashes("Tracy is cool.");
+//    }
+//
+//    public static void addDashes(String original){
+//
+//    String[] splitBySpaces = original.split(" ");
+//
+//        System.out.println(Arrays.toString(splitBySpaces));
+//
+//    for (String string : splitBySpaces) {
+//        String[] splitString = splitBySpaces.split;
+//    }
+////
+////    String newString = String.join("-", splitString);
+////
+////
+////        return newString;
+//
+//    }
+//}
