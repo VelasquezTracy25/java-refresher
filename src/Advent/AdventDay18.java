108fe67e6e882e51db4fa472852a1608b31cfa5c
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-12-20 20:02:33 -0600

    setup for day 17

---
+++ src/Advent/AdventDay18.java
@@ -0,0 +1,13 @@
+package Advent;
+
+public class AdventDay18 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] returnArrayLess(int[] numbers){
+
+
+    return newList;}
+}
