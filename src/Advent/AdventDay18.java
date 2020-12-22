898091d3f88fdfb436a103e6259c108581c8e962
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-12-22 17:04:54 -0600

    class review

--- src/Advent/AdventDay18.java
+++ src/Advent/AdventDay18.java
@@ -1,13 +1,22 @@
-package Advent;
-
-public class AdventDay18 {
-
-    public static void main(String[] args) {
-
-    }
-
-    public static int[] returnArrayLess(int[] numbers){
-
-
-    return newList;}
-}
+//package Advent;
+//
+//public class AdventDay18 {
+//
+//    public static void main(String[] args) {
+//
+//    }
+//
+//    public static int[] returnArrayLess(int[] numbers){
+//
+//    int[] newList = []
+//
+//    for (int i=0; i<numbers.length; i++){
+//    if (numbers[i-1]<i){
+//    return i-1;
+//    }
+//
+//
+//    }
+//
+//    return newList;}
+//}
