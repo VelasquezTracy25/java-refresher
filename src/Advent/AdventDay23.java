30fdc0d6159746126e758f2262c41e1ee1d620be
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-12-23 23:20:35 -0600

    setup

--- src/Advent/AdventDay23.java
+++ src/Advent/AdventDay23.java
@@ -1,4 +1,16 @@
-package Advent;
+// Count character from user input. Show characters used vs. characters remaining.
+
+//Twitter max character count = 280
+
+ package Advent;
 
 public class AdventDay23 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void tweetCharacterCount(String tweet){
+    }
+
 }
