dcea91ff8a5830ee930c1433e3053f58afac42a6
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-12-23 23:27:51 -0600

    solution

--- src/Advent/AdventDay23.java
+++ src/Advent/AdventDay23.java
@@ -2,15 +2,28 @@
 
 //Twitter max character count = 280
 
- package Advent;
+package Advent;
 
 public class AdventDay23 {
 
     public static void main(String[] args) {
-
+        tweetCharacterCount("This is my first tweet.");
+        tweetCharacterCount("This tweet should be longer than 280 characters. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec lorem interdum, luctus eros eget, gravida felis. Quisque ut interdum turpis. Vestibulum dolor arcu, luctus vel nibh sed, tempor convallis tortor. Fusce in diam blandit, fermentum quam ac, lobortis tortor integer.");
+        tweetCharacterCount("This is my final tweet ever!");
     }
 
-    public static void tweetCharacterCount(String tweet){
+    public static void tweetCharacterCount(String tweet) {
+        int characterLimit = 280;
+
+        if (tweet.length() < 280) {
+            System.out.println("Your tweet: " + tweet);
+            System.out.println("Characters used: " + tweet.length());
+            System.out.println("Characters left: " + (280 - tweet.length()));
+        } else {
+            System.out.println();
+            System.out.println("Your tweet '" + tweet + "' exceeds the character limit.");
+        }
+
     }
 
 }
