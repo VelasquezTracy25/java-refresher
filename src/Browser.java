2d37b777d2bb620cd7eb5a67a3941955d399fc73
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-12-28 19:52:14 -0600

    playing with shortcuts

--- src/Browser.java
+++ src/Browser.java
@@ -2,7 +2,12 @@ public class Browser {
     public void navigate(String address) {
         String ip = findIpAddress(address);
         String html = sendHttpRequest(ip);
+        String testVar = testMethod(address);
         System.out.println(html);
+    }
+
+    private String testMethod(String address) {
+    return "test string";
     }
 
     private String sendHttpRequest(String ip) {
