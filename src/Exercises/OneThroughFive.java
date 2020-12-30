8011453191e6f7a26c44087fdde796966f45897f
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-12-29 23:39:21 -0600

    Fibonnaci practice

--- src/Exercises/OneThroughFive.java
+++ src/Exercises/OneThroughFive.java
@@ -2,15 +2,24 @@ package Exercises;
 
 public class OneThroughFive {
     public static void main(String[] args) {
-
-        System.out.println(isAPrimeNumber(5));
+        System.out.println(fibonnaci(5));
+        System.out.println(fibonnaci(3));
+        System.out.println(isPrimeNumber(5));
     }
 
-    public static boolean isAPrimeNumber(int number) {
+    public static boolean isPrimeNumber(int number) {
         if (number % 1 == 0) {
             return true;
         }
         return false;
+    }
+
+    public static long fibonnaci(int i){
+    if (i == 0) return 0;
+    if (i <= 2) return 1;
+
+    long fibTerm = fibonnaci(i-1) + fibonnaci(i-2);
+    return fibTerm;
     }
 
 }
