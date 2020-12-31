23798ca0d94104dbc4df662cb375f01b0ca79839
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-12-30 20:30:08 -0600

    First iteration of game, return random word from list

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -1,2 +1,25 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class RockPaperScissors {
+
+    public static void main(String[] args) {
+        rockPaperScissors();
+    }
+
+
+    public static void rockPaperScissors() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Rock, paper scissors, shoot!");
+        String playersResponse = input.nextLine();
+        List<String> list = Arrays.asList("Rock", "Paper", "Scissors");
+        Random rand = new Random();
+        String randomElement = list.get(rand.nextInt(list.size()));
+
+        System.out.println(randomElement);
+
+
+    }
 }
