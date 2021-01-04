229371680103773d84851cd867d305d1e4dddbea
Tracy Velasquez <velasqueztracy25@gmail.com>
2021-01-03 23:30:46 -0600

    Completed mini game. Added comments, and conditional for invalid moves

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -16,16 +16,25 @@ public class RockPaperScissors {
         String playersResponse = input.nextLine();
         List<String> list = Arrays.asList("Rock", "Paper", "Scissors");
         Random rand = new Random();
+        //Picks random indexed element using rand - this is the games move/choice
         String randomElement = list.get(rand.nextInt(list.size()));
 
-        if (playersResponse.equalsIgnoreCase(randomElement)) {
+
+        //Checks for invalid input
+        if (!playersResponse.equalsIgnoreCase("rock") && !playersResponse.equalsIgnoreCase("paper") && !playersResponse.equalsIgnoreCase("scissors")) {
+            System.out.println("Invalid move. Please enter rock, paper, or scissors.");
+            rockPaperScissors();
+        //Rules out if both players choose the same thing.
+        } else if (playersResponse.equalsIgnoreCase(randomElement)) {
             System.out.println("Draw! I chose " + randomElement + " too. Shoot again.");
             rockPaperScissors();
+        //If player chooses winning move, congratulate them
         } else if (
                 (playersResponse.equalsIgnoreCase("rock") && randomElement.equals("Scissors")) ||
                 (playersResponse.equalsIgnoreCase("paper") && randomElement.equals("Rock")) ||
                 (playersResponse.equalsIgnoreCase("scissors") && randomElement.equals("Paper"))) {
             System.out.println("Congrats, I chose " + randomElement + ". You win.");
+        //Catches when player does not win
         } else {
             System.out.println("Sorry, I chose " + randomElement + ". Try again.");
             rockPaperScissors();
