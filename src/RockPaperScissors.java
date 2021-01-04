5c6966d35dbb215c126e2185ff718164851c6407
Tracy Velasquez <velasqueztracy25@gmail.com>
2021-01-03 23:18:29 -0600

    Added sout for wins

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -18,13 +18,14 @@ public class RockPaperScissors {
         Random rand = new Random();
         String randomElement = list.get(rand.nextInt(list.size()));
 
-        if (randomElement.equalsIgnoreCase(playersResponse)) {
+        if (playersResponse.equalsIgnoreCase(randomElement)) {
             System.out.println("Draw! I chose " + randomElement + " too. Shoot again.");
             rockPaperScissors();
         } else if (
-                randomElement.equalsIgnoreCase("rock") && rand.equals("Scissors") ||
-                randomElement.equalsIgnoreCase("paper") && rand.equals("Rock") ||
-                randomElement.equalsIgnoreCase("scissors") && rand.equals("Paper")) {
+                (playersResponse.equalsIgnoreCase("rock") && randomElement.equals("Scissors")) ||
+                (playersResponse.equalsIgnoreCase("paper") && randomElement.equals("Rock")) ||
+                (playersResponse.equalsIgnoreCase("scissors") && randomElement.equals("Paper"))) {
+            System.out.println("Congrats, I chose " + randomElement + ". You win.");
         } else {
             System.out.println("Sorry, I chose " + randomElement + ". Try again.");
             rockPaperScissors();
