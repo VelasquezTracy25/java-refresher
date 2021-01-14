547016ea34326f0a900e9fe30a7b98b5282f46b9
Tracy Velasquez <velasqueztracy25@gmail.com>
2021-01-13 22:56:38 -0600

    First draft of challenge solution

--- src/CoinChallenge.java
+++ src/CoinChallenge.java
@@ -1,6 +1,35 @@
 //revisiting coin question
 // with coins 3, 5, and 1, list the minimimum amount of coins needed to return change
 
+import java.util.*;
 
- public class CoinChallenge {
+public class CoinChallenge {
+    public static void main(String[] args) {
+        minCoins(43);
+
+    }
+
+    public static int minCoins(int input) {
+        int[] coins = {1, 3, 5};
+
+// initialize total coins
+        int totalCoins = 0;
+
+        while (input != 0) {
+            if (input - coins[0] >= 0) {
+                totalCoins++;
+                input = input - coins[0];
+            } else if (input - coins[1] >= 0) {
+                totalCoins++;
+                input = input - coins[1];
+            } else if (input - coins[2] >= 0) {
+                totalCoins++;
+                input = input - coins[2];
+            }
+        }
+        return totalCoins;
+    }
+
+}
+
 }
