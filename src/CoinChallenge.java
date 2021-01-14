2a6991721472430c5d6df0c290e682b057d6961f
Tracy Velasquez <velasqueztracy25@gmail.com>
2021-01-13 22:57:51 -0600

    tests for solution

--- src/CoinChallenge.java
+++ src/CoinChallenge.java
@@ -1,12 +1,14 @@
 //revisiting coin question
-// with coins 3, 5, and 1, list the minimimum amount of coins needed to return change
+// with coins 3, 5, and 1, list the minimum amount of coins needed to return change
 
 import java.util.*;
 
 public class CoinChallenge {
     public static void main(String[] args) {
-        minCoins(43);
-
+        System.out.println(minCoins(11)); //should return 3 (5+5+1)
+        System.out.println(minCoins(20)); // should return 4 (5x4)
+        System.out.println(minCoins(1)); // should return 1 (1)
+        System.out.println(minCoins(0)); // should return 0
     }
 
     public static int minCoins(int input) {
@@ -29,7 +31,5 @@ public class CoinChallenge {
         }
         return totalCoins;
     }
-
-}
 
 }
