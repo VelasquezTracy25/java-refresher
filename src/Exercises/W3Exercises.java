0cdc4ddf4a0cc2857d140f4130416af35a1220d5
Tracy Velasquez <velasqueztracy25@gmail.com>
2021-02-06 22:00:40 -0600

    solution for exercise

--- src/Exercises/W3Exercises.java
+++ src/Exercises/W3Exercises.java
@@ -53,5 +53,13 @@ public class W3Exercises {
         return sum;
     }
 
+    //Write a Java program to print the odd numbers from 1 to 99. Prints one number per line.
 
+public static void printNumbers(int number){
+    for (int i = 1; i < 100; i++) {
+        if (i % 2 != 0) {
+            System.out.println(i);
+        }
+    }
+    }
 }
